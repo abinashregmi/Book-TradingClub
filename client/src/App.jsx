dee080eadc435500cd2f1ee26a59634e2f58d98f
Abinash Regmi <abinashregmi01234@gmail.com>
2026-01-29 23:45:32 +0545

    making navigation bars

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,10 +4,13 @@ import Home from './pages/Home';
 import Signin from './pages/Signin';
 import About from './pages/About'
 import Profile from './pages/Profile';
+import Header from './components/Header';
+
 
 export default function App() {
   return (
     <BrowserRouter>
+    <Header/>
     <Routes>
       <Route path='/' element={<Home/>}/>
       <Route path='/sign-in' element={<Signin/>}/>
