db74370ea545168dd573bc4ec270fb43453197af
Abinash Regmi <abinashregmi01234@gmail.com>
2026-02-07 16:31:16 +0545

    handling error by backend server

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,25 +1,7 @@
-import {BrowserRouter,Routes, Route} from 'react-router-dom';
-import SignUp from './pages/SignUp';
-import Home from './pages/Home';
-import Signin from './pages/Signin';
-import About from './pages/About'
-import Profile from './pages/Profile';
-import Header from './components/Header';
-
+import React from 'react'
 
 export default function App() {
   return (
-    <BrowserRouter>
-    <Header/>
-    <Routes>
-      <Route path='/' element={<Home/>}/>
-      <Route path='/sign-in' element={<Signin/>}/>
-      <Route path='/sign-up' element={<SignUp/>}/>
-      <Route path='/about' element={<About/>}/>
-      <Route path='/profile' element={<Profile/>}/>
-    </Routes>
-    
-    
-    </BrowserRouter>
+    <h1 className='text-red-500'>App</h1>
   )
 }
