13852ac148146910561f58123df9ec4b568ad167
Abinash Regmi <abinashregmi01234@gmail.com>
2026-02-07 17:26:37 +0545

    successfully recovered pages and components folder

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,8 +1,15 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import tailwindcss from '@tailwindcss/vite'
+import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [tailwindcss(), react()],
+  plugins: [react()],
+  server: {
+    port: 3000,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5000',
+        changeOrigin: true,
+      }
+    }
+  }
 })
